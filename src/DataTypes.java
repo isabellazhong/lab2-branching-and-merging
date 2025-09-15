174bae0dcd723bac6ddaae16de42eb225a77d265
Isabella Zhong <isabellazhong888@gmail.com>
2025-09-15 11:47:21 -0400

    task_1 complete

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,11 +1,15 @@
 public class DataTypes {
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
+    public static void main(String[] args) {
+        sum(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+    }
+
     public static long sum(int[] numbers) {
-        int s = 0; // variable to accumulate the sum in!
+        long s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
         }
+        System.out.println(s);
         return s;
     }
 }
